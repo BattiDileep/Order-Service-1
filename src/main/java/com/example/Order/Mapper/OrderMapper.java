b39b08a82c870d93bbeb8af7625b23fa9f7948f3
dileep <battidileep6@gmail.com>
2024-06-04 23:13:14 +0530

    order mapper class implemented

--- src/main/java/com/example/Order/Mapper/OrderMapper.java
+++ src/main/java/com/example/Order/Mapper/OrderMapper.java
@@ -6,10 +6,18 @@ import com.example.Order.Entity.Order;
 public class OrderMapper {
 	public static OrderDTO mapToOrderDTO(Order order) {
 		
-		return null;
+		return new OrderDTO(
+				order.getOrderId(),
+				order.getOrderName(),
+				order.getOrderQty(),
+				order.getOrderDeliveryLocation());
 	}
 	public static Order mapToOrder(OrderDTO orderdto) {
-		return null;
+		return new Order(
+				orderdto.getOrderId(),
+				orderdto.getOrderName(),
+				orderdto.getOrderQty(),
+				orderdto.getOrderDeliveryLocation());
 	}
 
 }
